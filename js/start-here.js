e89174de803c138b841a0145158c1ee105536458
Bryan Barreto <barretobryan@hotmail.com>
2023-02-01 09:31:22 -0800

    final

--- js/start-here.js
+++ js/start-here.js
@@ -1,7 +1,8 @@
-$("document").ready(function(){
-    $('.seasons a').click(function(e){//find all a tags inside class of seasons
-        e.preventDefault();//stop default submission
-        var season = $(this).attr("href");//contents of href attribute of this element
-        alert(season);
-    });
+$("document").ready(function () {
+	$(".seasons a").click(function (e) {
+		//find all a tags inside class of seasons
+		e.preventDefault(); //stop default submission
+		var season = $(this).attr("href"); //contents of href attribute of this element
+		alert(season);
+	});
 });
